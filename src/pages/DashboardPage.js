cba74e7b9067a86640921fdc8ea8694fc9068c4f
dulminiNureka2001 <dulmininureka2001@gmail.com>
2025-07-24 09:55:05 +0530

    changed

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -48,17 +48,6 @@ const DashboardPage = () => {
         </svg>
       ),
       color: 'green'
-    },
-    {
-      title: 'System Health',
-      value: '99.9%',
-      change: '+0.1% from yesterday',
-      icon: (
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-        </svg>
-      ),
-      color: 'success'
     }
   ];
 
